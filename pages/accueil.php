819e78a06f87c736ecee2ce578154ea9d6395298
AurelienValat <aurelien.valat@iut-rodez.fr>
2024-12-10 09:08:32 +0100

    fix page d'accueil

--- pages/accueil.php
+++ pages/accueil.php
@@ -2,15 +2,24 @@
 <html lang="fr">
     <head>
         <meta charset="utf-8">  
-        <link rel="stylesheet" href="../css/index.css"/>
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
         <link href="../css/navBar.css" rel="stylesheet">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
         <title>MUSEOFLOW - Acceuil</title>
     </head>
     
     <body class="fond">
-        <?php
-        // Barre de navigation
-        include "navBar.php";
-        ?>
+        <nav class="navbar">
+            <div class="logo">
+                <a href="..\index.php"><img class="logo-img" src="../ressources/images/logo.png" alt="Logo MuseoFlow"></a>
+                Intranet du Musée
+            </div>
+            <div class="main-menu">
+                <div class="menu-item">Utilisateurs</div>
+                <div class="menu-item">Expositions</div>
+                <div class="menu-item">Conférenciers</div>
+                <div class="menu-item">Visites</div>
+                <div class="menu-item">Exportation</div>
+                <div class="menu-item">Déconnexion</div>
+            </div>
+        </nav>
 </html>
