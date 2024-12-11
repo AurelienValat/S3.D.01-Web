e0b8d81848ef44d1732981688b094622f124b4e3
Neidrow <amjed.sehil@iut-rodez.fr>
2024-12-11 09:04:23 +0100

    page visite fix

--- pages/visites.php
+++ pages/visites.php
@@ -7,31 +7,25 @@
     <meta charset="utf-8">  
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link href="../css/visites.css" rel="stylesheet">
+    <link href="../ressources\fontawesome-free-6.5.1-web\css\all.css" rel="stylesheet"></link>
     <title>MUSEOFLOW - Gestion des Visites</title>
 </head>
 
 <body class="fond">
-    <nav class="navbar navbar-expand-lg">
-        <div class="container-fluid">
+<nav class="navbar">
             <div class="logo">
-                <a href="..\index.php"><img class="logo-img" src="../ressources/images/logo.png" alt="Logo MuseoFlow"></a>
-                <div class="policeBlanche"> Intranet du Musée </div>
+                <a href="accueil.php"><img class="logo-img" src="../ressources/images/logo.png" alt="Logo MuseoFlow"></a>
+                Intranet du Musée
             </div>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav ms-auto">
-                    <li class="nav-item"><a class="nav-link" href="#">Utilisateurs</a></li>
-                    <li class="nav-item"><a class="nav-link" href="#">Expositions</a></li>
-                    <li class="nav-item"><a class="nav-link" href="#">Conférenciers</a></li>
-                    <li class="nav-item"><a class="nav-link" href="#">Visites</a></li>
-                    <li class="nav-item"><a class="nav-link" href="#">Exportation</a></li>
-                    <li class="nav-item"><a class="nav-link" href="#">Déconnexion</a></li>
-                </ul>
+            <div class="main-menu">
+                <div class="menu-item">Utilisateurs</div>
+                <div class="menu-item">Expositions</div>
+                <div class="menu-item">Conférenciers</div>
+                <a href="visites.php"> <div class="menu-item">Visites</div> </a>
+                <div class="menu-item">Exportation</div>
+                <a href="deconnexion.php"><div class="menu-item">Déconnexion</div></a>
             </div>
-        </div>
-    </nav>
+        </nav>
 
     <div class="container content">
     <div class="container-blanc">
@@ -39,14 +33,13 @@
         <div class="d-flex justify-content-between align-items-center">
             <button class="btn-red">Ajouter/Réserver une visite</button>
             <button class="btn btn-light d-flex align-items-center gap-2">
-                <img src="../ressources/images/filtre-icon.png" alt="Filtres" width="20">
-                Filtres
+            <i class="fa-solid fa-filter"></i>                Filtres
             </button>
         </div>
 
         <div class="table">
             <table class="table table-striped table-bordered">
-                <thead class="table-striped">
+                <thead class="table-dark">
                     <tr>
                         <th>Date</th>
                         <th>Heure</th>
